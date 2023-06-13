dbd0df37618975f990168482fe562b1fb99f71df
gusalbukrk <gusalbukrk@gmail.com>
2023-06-13 12:26:09 -0300

    add `react-router`

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import db from './db.json';
 
 import { useState } from 'react';
+import { Routes, Route, useParams } from 'react-router-dom';
 
 const getRandomArrayItem = arr => arr[Math.floor(Math.random() * arr.length)];
 
@@ -13,30 +14,68 @@ function getRandomQuote() {
 }
 
 function App() {
-  const [quotes, setQuotes] = useState([ getRandomQuote() ]);
-
   return (
     <div className="App">
       <header>
         <h1>terrific quotes by terrible people</h1>
       </header>
+      <Routes>
+        <Route path='quotes'>
+          <Route path='' element={<Home />} />
+          <Route path='tags' element={<Tags />} />
+          <Route path='tags/:tag' element={<Tag />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Quote({ quote }) {
+  return (
+    <article className="quote-container">
+      <h2 className="quote"><span>“</span>{quote.quote}<span>”</span></h2>
+      <section className='author'>
+        <img src={require(`./pics/${quote.author}.jpg`)} alt={quote.author} />
+        <section>
+          <h3 className="authorName">– {quote.author}</h3>
+          <h4 className="bio">known for {quote.bio}</h4>
+        </section>
+      </section>
+      <section className="tags">{quote.tags.map((t, i) => <span key={i}>{t}</span>)}</section>
+      <hr></hr>
+    </article>
+  );
+}
+
+function Home() {
+  const [quotes, setQuotes] = useState([ getRandomQuote() ]);
+
+  return (
+    <main>
       {quotes.map((quote, index) => (
-        <article key={index} className="quote-container">
-          <h2 className="quote"><span>“</span>{quote.quote}<span>”</span></h2>
-          <section className='author'>
-            <img src={require(`./pics/${quote.author}.jpg`)} />
-            <section>
-              <h3 className="authorName">– {quote.author}</h3>
-              <h4 className="bio">known for {quote.bio}</h4>
-            </section>
-          </section>
-          <section className="tags">{quote.tags.map((t, i) => <span key={i}>{t}</span>)}</section>
-          <hr></hr>
-        </article>
+        <Quote key={index} quote={quote} />
       ))}
 
       <button id="more" onClick={() => setQuotes([...quotes, getRandomQuote()])}>Load 1 More</button>
-    </div>
+    </main>
+  );
+}
+
+function Tags() {
+  return (
+    <main>
+      <h2>Tags</h2>
+    </main>
+  );
+}
+
+function Tag() {
+  const { tag } = useParams();
+
+  return (
+    <main>
+      <h2>Tag - {tag}</h2>
+    </main>
   );
 }
 
